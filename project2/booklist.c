eb6fe172248293b71ca0be9340a1c0a26b16e858
williamgeorgeschaefer <30483286+williamgeorgeschaefer@users.noreply.github.com>
2020-11-11 11:18:28 -0500

    Working on List Code...

--- project2/booklist.c
+++ project2/booklist.c
@@ -10,6 +10,15 @@ struct book{
     struct book *header = 0;
     struct book *trailer = 0;
 
-    void add(struct book newBook){
-        newBook.isbn = isbn;
+    void add(struct book *newBook){
+        struct book *node = (struct book*)malloc(sizeof(struct book));
+
+        node->numCopies = 1;
+        node->isbn = newBook->isbn;
+        node->title = newBook->title;
+        node->last = newBook->last;
+        node->first = newBook->first;
+
+        node.next = newBook;
+        header = node;
     }
