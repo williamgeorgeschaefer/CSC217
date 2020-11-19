a1181b5d7f241a76c27e51b181926498fb33f27b
williamgeorgeschaefer <30483286+williamgeorgeschaefer@users.noreply.github.com>
2020-11-19 09:40:06 -0500

    New Files Being Created...

---
+++ project2/createbook.c
@@ -0,0 +1,11 @@
+// William Schaefer (N00857559@students.ncc.edu)
+// CSC 217
+// Project 2: "Bookstore Part 1"
+// Due November 25, 2020 at 11:59 pm
+// createbook.c
+//
+// This code reads in lines from standard input representing
+// books being shipped to the bookstore.  It creates structures
+// containing information on a particular book, and stores them
+// in a linked list.  It also accepts and rejects lines based on
+// the validity of what is passed in.
